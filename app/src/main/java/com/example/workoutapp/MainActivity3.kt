61c35afcf06fe4d1f8979f81b0a1a469a0fae27f
RehanGith <chrehan1007@gmail.com>
2024-10-14 12:56:39 +0500

    Used the Recycle view layout mangers, Grid, liner, horizontal

--- app/src/main/java/com/example/workoutapp/MainActivity3.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity3.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity3 : AppCompatActivity() {
@@ -22,6 +23,7 @@ class MainActivity3 : AppCompatActivity() {
             insets
         }
         view = findViewById<RecyclerView>(R.id.RecyclerView)
+        view.layoutManager = GridLayoutManager(this, 2)
         AdapterInit()
 
     }
